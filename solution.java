b7bf07e43d55467473e0c20513fcf23386e3b657
Alp Karar <88462979+AlpKarar@users.noreply.github.com>
2022-02-19 01:09:02 +0300

    Update solution.java

--- solution.java
+++ solution.java
@@ -15,10 +15,11 @@ public class Main
         
         int result = (int)(Math.random() * 100);
         
-        int guess;
+        int guess, numOfGuess = 0;
         
+        System.out.println("You can make 5 guesses. Good Luck!");
         
-        while (true) {
+        while (numOfGuess <= 5) {
             System.out.print("Enter a number in range of 0 and 100: ");
             guess = input.nextInt();
             
@@ -26,8 +27,27 @@ public class Main
                 System.out.print("YOU WIN!!!");
                 break;
             } else {
-                System.out.print("Please try again!");
+                numOfGuess +=1;
+                if (4 < numOfGuess) {
+                System.out.println("Sorry, GAME OVER!!!");
+                break;
+                }
+                System.out.println("Please try again!");
+                if (result < guess) {
+                    if (result <= (guess - 5)) {
+                        System.out.println("Reduce your guess by 5 or more!");
+                    } else {
+                        System.out.println("Reduce your guess by 5 or less!");
+                    }
+                } else if (guess < result) {
+                    if ((guess + 5) < result) {
+                        System.out.println("Increase your guess by 5 or more!");
+                    } else {
+                        System.out.println("Increase your guess by 5 or less!");
+                    }
+                }
             }
         }
     }
 }
+
